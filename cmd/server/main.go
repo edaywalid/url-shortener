a6936448e9ae6d473d3fe197fb11c26c0948d2b5
edaywalid <becharwalid9@gmail.com>
2024-09-27 18:24:21 +0100

    set up the project

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Info().Msg("Hello World")
+}
