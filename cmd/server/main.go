0a0b51e0f6d927e874b91048cc994dc4bb9994d1
edaywalid <becharwalid9@gmail.com>
2024-10-04 08:20:40 +0100

    starting the app server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/edaywalid/url-shortner/internal/app"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -10,7 +13,13 @@ import (
 func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	log.Info().Msg("Hello World")
+	log.Info().Msg("Starting the application")
+	app, err := app.NewApp()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create the app")
+		return
+	}
+
 	go func() {
 		stopChan := make(chan os.Signal, 1)
 		signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
@@ -22,4 +31,10 @@ func main() {
 
 		os.Exit(0)
 	}()
+
+	if err := app.Init(); err != nil {
+		log.Error().Err(err).Msg("Failed to initialize the app")
+		return
+	}
+
 }
