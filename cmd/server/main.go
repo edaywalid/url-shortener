47ff00b41b12f3a20e42f858adcb85c365121766
edaywalid <becharwalid9@gmail.com>
2024-10-04 08:08:14 +0100

    add a stop goroutine

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,4 +11,15 @@ func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	log.Info().Msg("Hello World")
+	go func() {
+		stopChan := make(chan os.Signal, 1)
+		signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+		<-stopChan
+
+		log.Info().Msg("Shutting down the server...")
+		app.Close()
+		log.Info().Msg("Server gracefully stopped")
+
+		os.Exit(0)
+	}()
 }
